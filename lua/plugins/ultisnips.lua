8e9c719cfef0bbfe581234d645fd7ab8c35add46
tomnycui <tomnycui@126.com>
2024-12-16 13:26:51 +0800

    change all snippets to be suffix triggered

--- lua/plugins/ultisnips.lua
+++ lua/plugins/ultisnips.lua
@@ -1,11 +1,11 @@
 ---@type LazySpec
 
 return {
-    "SirVer/ultisnips",
-    init = function()
-        -- VimTeX configuration goes here, e.g.
-        vim.g.UltiSnipsExpandTrigger = "<tab>"
-        vim.g.UltiSnipsJumpForwardTrigger = "<c-[>"
-        vim.g.UltiSnipsJumpBackwardTrigger = "<c-]>"
-    end,
+  "SirVer/ultisnips",
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.UltiSnipsExpandTrigger = ";"
+    vim.g.UltiSnipsJumpForwardTrigger = "<A-]>"
+    vim.g.UltiSnipsJumpBackwardTrigger = "<A-[>"
+  end,
 }
